50de5b6bfb086c5e50b8a15e0c665fe2970f14dd
zmar234 <zachmarino234@gmail.com>
2024-12-22 13:03:40 -0500

    Add presentation link and small transition to header

--- src/pages/About.tsx
+++ src/pages/About.tsx
@@ -5,6 +5,8 @@ export const About = () => {
     return (
         <div>
             <h1>About</h1>
+
+            <iframe  className="w-full p-9" style={{border: '1px solid rgba(0, 0, 0, 0.1)'}} height="750" src="https://embed.figma.com/slides/H1QxQ68WMjtA6wrn0egYWp/Pitch-Project-Presentation?node-id=1-587&embed-host=share"></iframe>
         </div>
     );
 };
