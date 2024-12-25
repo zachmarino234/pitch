d0798eed4a5a088ef3d54b155a1ee4f9fefb882a
zmar234 <zachmarino234@gmail.com>
2024-12-25 17:44:43 -0500

    Case Studies page

---
+++ src/components/ArticleCard.tsx
@@ -0,0 +1,37 @@
+const cardTypeColors: { [key: string]: string } = {
+    game: 'bg-game-red',
+    prompt: 'bg-prompt-teal',
+    topic: 'bg-topic-yellow',
+    // Add more card types and their corresponding colors here
+};
+
+export const ArticleCard = ({
+    headline,
+    author,
+    cardsUsed,
+    cardType,
+    link
+}: {
+    headline: string;
+    author: string;
+    cardsUsed: string[];
+    cardType: string[];
+    link: string;
+}) => {
+    return (
+        <a href={link} target="_blank" rel="noopener noreferrer" className="flex flex-col gap-2 p-4 rounded-xl border-2 no-underline text-black hover:text-inherit">
+            <h2 className="font-richmond text-2xl font-semibold">{headline}</h2>
+            <div className="flex flex-col gap-4 sm:flex-row justify-between items-baseline">
+                <span>{author}</span>
+                <div className="flex gap-2 items-start">
+
+                    {cardsUsed.map((card, index) => (
+                        <span key={index} className={` text-white px-2 py-1 rounded ${cardTypeColors[cardType[index]] || 'bg-gray-200'}`}>
+                            {card}
+                        </span>
+                    ))}
+                </div>
+            </div>
+        </a>
+    );
+}
--- src/pages/CaseStudies.tsx
+++ src/pages/CaseStudies.tsx
@@ -1,10 +1,20 @@
+import { ArticleCard } from "../components/ArticleCard";
+import GlobeLogo from '../assets/The_Boston_Globe.svg'
 
 
 export const CaseStudies = () => {
 
     return (
-        <div>
-            <h1>Case Studies</h1>
+        <div className="mt-5 sm:mx-36">
+            <p className="mb-5 font-semibold">Want a story you wrote featured? <a href="mailto:zachmarino234@gmail.com">Email me</a> with the story and the cards you used to develop the idea!</p>
+            <img src={GlobeLogo} alt="The Boston Globe Logo" className="w-1/2 sm:w-1/4 mb-3" />
+            <ArticleCard
+                headline="Video game designer finds ‘Sweet Spot’ through Boston Public Art Triennial Accelerator program"
+                author="Emily Wyrwa"
+                cardsUsed={['Pair Programming', 'Variety']}
+                cardType={['game', 'prompt']}
+                link="https://www.bostonglobe.com/2024/12/20/arts/sweet-spot-boston-public-art-triennial-accelerator/" 
+            />
         </div>
     );
 };
